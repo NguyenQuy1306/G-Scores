29be89d1b49e948c789c970592d883fadb91458f
nguyen quy <nngocquy75@gmail.com>
2025-06-10 23:05:48 +0700

    test reset

---
+++ g-score.client/src/components/common/SideBar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="sidebar">
+    <div class="menu">
+      <h3>Menu</h3>
+      <ul>
+        <li v-for="item in items" :key="item.id" :class="{ active: isActive(item.route) }">
+          <router-link :to="item.route">{{ item.name }}</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
+function isActive(route: string): boolean {
+  return route.toLowerCase() === route.toLowerCase();
+}
+const items = ref([
+  { id: 1, name: 'Home', route: '/' },
+  { id: 2, name: 'About', route: '/About' },
+]);
+</script>
+<style scoped>
+</style>
